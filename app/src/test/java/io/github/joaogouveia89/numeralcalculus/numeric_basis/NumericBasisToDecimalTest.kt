e3117522be1bb57574d97e42691869024a85e45b
Joao Gouveia <joao.gouveia@criticaltechworks.com>
2020-02-18 22:04:52 +0000

    added the tests for convertion to base 10

---
+++ app/src/test/java/io/github/joaogouveia89/numeralcalculus/numeric_basis/NumericBasisToDecimalTest.kt
@@ -0,0 +1,65 @@
+package io.github.joaogouveia89.numeralcalculus.numeric_basis
+
+import android.util.Log
+import io.github.joaogouveia89.numeralcalculus.calculus.numeric_basis.NumericBasisToDecimal
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.powermock.api.mockito.PowerMockito
+import org.powermock.api.mockito.PowerMockito.mock
+import org.powermock.core.classloader.annotations.PrepareForTest
+import org.powermock.modules.junit4.PowerMockRunner
+
+@RunWith(PowerMockRunner::class)
+@PrepareForTest(Log::class)
+class NumericBasisToDecimalTest{
+
+    @Before
+    fun beforeTest() {
+        PowerMockito.mockStatic(Log::class.java)
+        Mockito.`when`(Log.i(any(), any())).then {
+            println(it.arguments[1] as String)
+            1
+        }
+    }
+
+    @Test
+    fun binToDecTest(){
+        val listener = mock(NumericBasisToDecimal.Base10ConversionListener::class.java)
+        val bin = NumericBasisToDecimal("100", 2, listener)
+        bin.run()
+
+        verify(listener, times(1)).isDone("4")
+    }
+
+    @Test
+    fun octToDecTest(){
+        val listener = mock(NumericBasisToDecimal.Base10ConversionListener::class.java)
+        val oct = NumericBasisToDecimal("11345", 8, listener)
+        oct.run()
+
+        verify(listener, times(1)).isDone("4837")
+    }
+
+    @Test
+    fun hexToDecTest(){
+        val listener = mock(NumericBasisToDecimal.Base10ConversionListener::class.java)
+        val hex = NumericBasisToDecimal("FF", 16, listener)
+        hex.run()
+
+        verify(listener, times(1)).isDone("255")
+    }
+
+    @Test
+    fun outBoundTest(){
+        val listener = mock(NumericBasisToDecimal.Base10ConversionListener::class.java)
+        val ob = NumericBasisToDecimal("FFFAABBA", 16, listener)
+        ob.run()
+
+        verify(listener, times(1)).outBound()
+    }
+}
